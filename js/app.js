e0c8795e9686a794b98c21b8a297568f9bd4e689
Mrdeft2231 <116429749+Mrdeft2231@users.noreply.github.com>
2024-01-11 22:06:38 +0300

    Add

---
+++ js/app.js
@@ -0,0 +1,18 @@
+document.addEventListener('DOMContentLoaded', function() {
+    let removeElement = document.querySelector('.Load');
+    window.addEventListener('load', function() {
+        removeElement.remove();
+    });
+    
+});
+
+document.addEventListener('mousemove', e => {
+    Object.assign(document.documentElement, {
+        style: `
+        --move-x: ${(e.clientX - window.innerWidth / 2) * -.005}deg;
+        --move-y: ${(e.clientY - window.innerHeight / 2) * -.005}deg;
+        `
+    })
+})
+
+
