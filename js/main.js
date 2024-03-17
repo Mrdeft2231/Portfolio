1bcb901393f3b5016169b3a268f76326d3309a9a
Mrdeft2231 <116429749+Mrdeft2231@users.noreply.github.com>
2024-03-17 22:06:45 +0300

    add

--- js/main.js
+++ js/main.js
@@ -1,11 +1,16 @@
 
 const elementHeader = document.querySelector(".header");
 const elementChapter = document.querySelector(".chapter");
+const elementNavigation = document.querySelector(".nav__fixed");
+
 
 window.addEventListener("scroll", function() {
   if (window.scrollY >= elementChapter.offsetTop) {
     elementHeader.classList.add("hidden");
+    elementNavigation.classList.remove("hidden");
+    this.document.getElementById("nav__hidden").style.display = "block";
   } else {
+    this.document.getElementById("nav__hidden").style.display = "none";
     elementHeader.classList.remove("hidden");
   }
 });
